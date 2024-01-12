e3f3afd99f4c4b646d45367efe93e51d015ae923
dylan <dwelch@aerospike.com>
2024-01-11 16:53:54 -0800

    work on asb parser

--- unmarshal/asb.go
+++ unmarshal/asb.go
@@ -14,6 +14,13 @@ const (
 	records
 )
 
+const (
+	namespaceToken      = "namespace"
+	firstFileToken      = "first-file"
+	globalSectionMarker = "*"
+	maxNamespaceLength  = 31
+)
+
 type CountingByteReader struct {
 	io.ByteReader
 	count uint64
@@ -51,6 +58,8 @@ func (r *ASBReader) NextToken() (any, error) {
 	switch b {
 	case '#':
 		return readMetadata(r.source)
+	case '*':
+		return readGlobal(r.source)
 
 	default:
 		return nil, fmt.Errorf("read invalid asb line start character %s", b)
@@ -97,7 +106,7 @@ func readNamespace(src io.ByteReader) (string, error) {
 	}
 
 	if b != '#' {
-		return "", fmt.Errorf("invalid character %s in metadata section", b)
+		return "", fmt.Errorf("invalid character %b in metadata section namespace line", b)
 	}
 
 	b, err = src.ReadByte()
@@ -106,7 +115,23 @@ func readNamespace(src io.ByteReader) (string, error) {
 	}
 
 	if b != ' ' {
-		return "", fmt.Errorf("invalid character %s in metadata section", b)
+		return "", fmt.Errorf("invalid character %b in metadata section namespace line", b)
+	}
+
+	namespaceLen := len(namespaceToken)
+	bytes := make([]byte, namespaceLen)
+	for i := 0; i < namespaceLen; i++ {
+		var err error
+		bytes[i], err = src.ReadByte()
+		if err != nil {
+			return "", err
+		}
+	}
+
+	namespace := string(bytes)
+
+	if namespace != namespaceToken {
+		return "", fmt.Errorf("invalid namespace token %s in metadata namespace line", namespaceToken)
 	}
 
 	b, err = src.ReadByte()
@@ -114,14 +139,141 @@ func readNamespace(src io.ByteReader) (string, error) {
 		return "", err
 	}
 
-	firstFileLen := len("first-file")
-	bytes := make([]byte, versionTextLen)
-	for i := 0; i < firstFileLen; i++ {
+	if b != ' ' {
+		return "", fmt.Errorf("invalid character %b in metadata namespace line", b)
+	}
+
+	bytes = []byte{}
+	var count int
+	for {
+		// TODO support escaped namespaces?
+
+		b, err = src.ReadByte()
+		if err != nil {
+			return "", err
+		}
+		count++
+
+		if b == byte('\n') {
+			break
+		}
+
+		bytes[count] = b
+	}
+
+	return string(bytes), nil
+}
+
+func readFirst(src io.ByteReader) (bool, error) {
+	b, err := src.ReadByte()
+	if err != nil {
+		return false, err
+	}
+
+	// The first-file metadata line is optional
+	if b != '#' {
+		return false, nil
+	}
+
+	b, err = src.ReadByte()
+	if err != nil {
+		return false, err
+	}
+
+	if b != ' ' {
+		return false, fmt.Errorf("invalid character %b in metadata section first-file line", b)
+	}
+
+	firstLen := len(firstFileToken)
+	bytes := make([]byte, firstLen)
+	for i := 0; i < firstLen; i++ {
 		var err error
 		bytes[i], err = src.ReadByte()
 		if err != nil {
-			return nil, err
+			return false, err
 		}
 	}
 
+	first := string(bytes)
+
+	if first != firstFileToken {
+		return false, fmt.Errorf("invalid first-file token %s in metadata section first-file line", firstFileToken)
+	}
+
+	b, err = src.ReadByte()
+	if err != nil {
+		return false, err
+	}
+
+	if b != '\n' {
+		return false, fmt.Errorf("invalid character %b in metadata section first-file line", b)
+	}
+
+	return true, nil
+
+}
+
+type SIndexType byte
+
+const (
+	BinIndex         SIndexType = 'N'
+	ListElementIndex SIndexType = 'L'
+	MapKeyIndex      SIndexType = 'K'
+	MapValueIndex    SIndexType = 'V'
+)
+
+type SIDataType byte
+
+const (
+	InvalidSIDataType     SIDataType = 'I'
+	NumericSIDataType     SIDataType = 'N'
+	StringSIDataType      SIDataType = 'S'
+	GEO2DSphereSIDataType SIDataType = 'G'
+	BlobSIDataType        SIDataType = 'B'
+)
+
+type SecondaryIndex struct {
+	namespace     string
+	set           string
+	name          string
+	indexType     SIndexType
+	path          string
+	dataType      SIDataType
+	valuesCovered int
+}
+
+type UDFType byte
+
+const (
+	LUAUDFType UDFType = 'L'
+)
+
+type UDF struct {
+	udfType UDFType
+	name    string
+	length  uint32
+	content []byte
+}
+
+type Globals struct {
+	secondaryIndexes []SecondaryIndex
+	UDFs             []UDF
+}
+
+func readGlobals(src io.ByteReader) (*Globals, error) {
+	for {
+
+	}
+}
+
+// NOTE this is meant to read the UDF line AFTER the global start char *
+func readUDFs(src io.ByteReader) (*UDF, error) {
+	b, err := src.ReadByte()
+	if err != nil {
+		return nil, err
+	}
+
+	if b != ' ' {
+		return nil, fmt.Errorf("invalid character %b in global section UDF line", b)
+	}
 }

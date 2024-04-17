8c7c21a747fa916fad661af7deb4e8d8319a104c
Anton Korotkov <akorotkov@aerospike.com>
2024-04-17 15:59:37 +0300

    add integration test for s3

---
+++ s3_integration_test.go
@@ -0,0 +1,143 @@
+package backup_test
+
+import (
+	"context"
+	"github.com/docker/docker/api/types/image"
+	"io"
+	"testing"
+
+	"github.com/aerospike/backup-go"
+	"github.com/aerospike/backup-go/encoding"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/stretchr/testify/suite"
+)
+
+type writeReadTestSuite struct {
+	suite.Suite
+	docker  *client.Client
+	minioID string
+}
+
+func (s *writeReadTestSuite) SetupSuite() {
+	s.docker, _ = client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	ctx := context.Background()
+	_, err := s.docker.ImagePull(ctx, "minio/minio", image.PullOptions{})
+	if err != nil {
+		s.FailNow("could not pull minio image", err)
+	}
+
+	minioResponse, err := s.docker.ContainerCreate(ctx, &container.Config{
+		Image: "minio/minio",
+		Cmd:   []string{"server", "/data", "--console-address", ":9001"},
+		Env: []string{
+			"MINIO_ROOT_USER=minioadmin",
+			"MINIO_ROOT_PASSWORD=minioadmin",
+		},
+		ExposedPorts: nat.PortSet{
+			"9001/tcp": struct{}{},
+		},
+	}, &container.HostConfig{
+		PortBindings: nat.PortMap{
+			"9000/tcp": []nat.PortBinding{
+				{HostIP: "0.0.0.0", HostPort: "9000"},
+			},
+			"9001/tcp": []nat.PortBinding{
+				{HostIP: "0.0.0.0", HostPort: "9001"},
+			},
+		},
+	}, nil, nil, "minio")
+	if err != nil {
+		s.FailNow("could not create minio container", err)
+	}
+
+	s.minioID = minioResponse.ID
+	if err := s.docker.ContainerStart(ctx, s.minioID, container.StartOptions{}); err != nil {
+		s.FailNow("could not start minio container", err)
+	}
+
+	minioClient, err := minio.New("localhost:9000", &minio.Options{
+		Creds: credentials.NewStaticV4("minioadmin", "minioadmin", ""),
+	})
+	if err != nil {
+		s.FailNow("could not create minio client", err)
+	}
+
+	err = minioClient.MakeBucket(ctx, "backup", minio.MakeBucketOptions{})
+	if err != nil {
+		s.FailNow("could not create bucket", err)
+	}
+}
+
+func (s *writeReadTestSuite) TearDownSuite() {
+	ctx := context.Background()
+	_ = s.docker.ContainerRemove(ctx, s.minioID, container.RemoveOptions{Force: true})
+}
+
+func (s *writeReadTestSuite) TestWriteRead() {
+	config := &backup.S3Config{
+		Bucket:   "backup",
+		Region:   "eu",
+		Endpoint: "http://localhost:9000",
+		Profile:  "minio",
+	}
+
+	bytes := []byte{1, 2, 3, 4, 5}
+	namespace := "ns1"
+	s.write(namespace, bytes, config)
+
+	buffer, n := s.read(config)
+	s.Assertions.Equal(len(bytes), n)
+	s.Assertions.Equal(bytes, buffer[:n])
+}
+
+func (s *writeReadTestSuite) write(namespace string, bytes []byte, config *backup.S3Config) {
+	factory := backup.NewS3WriterFactory(config, encoding.NewASBEncoderFactory())
+
+	writer, err := factory.NewWriter(namespace, func(_ io.WriteCloser) error {
+		return nil
+	})
+	if err != nil {
+		s.FailNow("failed to create writer", err)
+	}
+
+	n, err := writer.Write(bytes)
+	if err != nil {
+		s.FailNow("failed to write", err)
+	}
+
+	s.Assertions.Equal(len(bytes), n)
+
+	err = writer.Close()
+	if err != nil {
+		s.FailNow("failed to close writer", err)
+	}
+}
+
+func (s *writeReadTestSuite) read(config *backup.S3Config) (buffer []byte, n int) {
+	factory := backup.NewS3ReaderFactory(config, encoding.NewASBDecoderFactory())
+
+	readers, err := factory.Readers()
+	if err != nil {
+		s.FailNow("failed to create readers", err)
+	}
+
+	s.Assertions.Equal(1, len(readers))
+
+	buffer = make([]byte, 10)
+	n, err = readers[0].Read(buffer)
+	if err != nil {
+		s.FailNow("failed to read", err)
+	}
+
+	return buffer, n
+}
+
+func TestReadWrite(t *testing.T) {
+	testSuite := writeReadTestSuite{}
+
+	suite.Run(t, &testSuite)
+}

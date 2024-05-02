5ebff49b589fa9c0467b62025b549a5c5cf3af31
Anton Korotkov <106995168+korotkov-aerospike@users.noreply.github.com>
2024-05-02 08:45:07 +0300

    Update reader_directory.go
    Co-authored-by: Eugene R. <yrizhkov@aerospike.com>

--- reader_directory.go
+++ reader_directory.go
@@ -53,7 +53,7 @@ func (f *DirectoryReaderFactory) Readers() ([]io.ReadCloser, error) {
 	}
 
 	if len(readers) == 0 {
-		return nil, fmt.Errorf("%w: %s doesn't contains backup files", ErrRestoreDirectoryInvalid, f.dir)
+		return nil, fmt.Errorf("%w: %s doesn't contain backup files", ErrRestoreDirectoryInvalid, f.dir)
 	}
 
 	return readers, nil

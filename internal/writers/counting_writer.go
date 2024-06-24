66b683e8f27e090c54ce9c036fd64dc38b698bd8
Anton Korotkov <akorotkov@aerospike.com>
2024-06-24 14:48:52 +0300

    add comments

--- internal/writers/counting_writer.go
+++ internal/writers/counting_writer.go
@@ -5,6 +5,8 @@ import (
 	"sync/atomic"
 )
 
+// CountingWriter counts total number of bytes written with it.
+// We need it to get actual number of bytes written, after compression and encryption.
 type CountingWriter struct {
 	writer io.WriteCloser
 	count  *atomic.Uint64
@@ -20,6 +22,7 @@ func NewCountingWriter(w io.WriteCloser, count *atomic.Uint64) *CountingWriter {
 func (cw *CountingWriter) Write(p []byte) (n int, err error) {
 	n, err = cw.writer.Write(p)
 	cw.count.Add(uint64(n))
+
 	return n, err
 }
 

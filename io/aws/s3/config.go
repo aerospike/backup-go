ded8e260a4ffe8a4778fda41889ebd863ce441dd
Dmitrii Neeman <dneeman@aerospike.com>
2024-08-01 17:52:25 +0300

    FMWK-516-refactoring-models - added s3 config constructor

--- io/aws/s3/config.go
+++ io/aws/s3/config.go
@@ -9,3 +9,22 @@ type Config struct {
 	Prefix    string
 	ChunkSize int
 }
+
+// NewConfig returns new AWS S3 configuration.
+func NewConfig(
+	bucket string,
+	region string,
+	endpoint string,
+	profile string,
+	prefix string,
+	chunkSize int,
+) *Config {
+	return &Config{
+		Bucket:    bucket,
+		Region:    region,
+		Endpoint:  endpoint,
+		Profile:   profile,
+		Prefix:    prefix,
+		ChunkSize: chunkSize,
+	}
+}

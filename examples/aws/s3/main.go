6f2d8179f7073d546b80333a186f858d7596a9c4
Dmitrii Neeman <dneeman@aerospike.com>
2024-08-22 11:26:58 +0300

    FMWK-531-contrext-revised - examples fix

--- examples/aws/s3/main.go
+++ examples/aws/s3/main.go
@@ -93,8 +93,10 @@ func runBackup(ctx context.Context, c *backup.Client) {
 		panic(err)
 	}
 
-	// use backupHandler.Wait() to wait for the job to finish or fail
-	err = backupHandler.Wait()
+	// Use backupHandler.Wait(ctx) to wait for the job to finish or fail.
+	// You can use different context here, and if it is canceled
+	// backupClient.Backup(ctx, backupCfg, writers) context will be cancelled too.
+	err = backupHandler.Wait(ctx)
 	if err != nil {
 		log.Printf("Backup failed: %v", err)
 	}
@@ -135,8 +137,10 @@ func runRestore(ctx context.Context, c *backup.Client) {
 		panic(err)
 	}
 
-	// use restoreHandler.Wait() to wait for the job to finish or fail
-	err = restoreHandler.Wait()
+	// Use restoreHandler.Wait(ctx) to wait for the job to finish or fail.
+	// You can use different context here, and if it is canceled
+	// backupClient.Restore(ctx, restoreCfg, streamingReader) context will be cancelled too.
+	err = restoreHandler.Wait(ctx)
 	if err != nil {
 		log.Printf("Restore failed: %v", err)
 	}

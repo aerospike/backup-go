a0efc84c46b4ce746fb3a16356cd887792f6c582
Dmitrii Neeman <dneeman@aerospike.com>
2024-08-22 13:47:51 +0300

    FMWK-538-gcp-support - update from main

--- examples/gcp/storage/main.go
+++ examples/gcp/storage/main.go
@@ -91,8 +91,10 @@ func runBackup(ctx context.Context, c *backup.Client) {
 		panic(err)
 	}
 
-	// use backupHandler.Wait() to wait for the job to finish or fail
-	err = backupHandler.Wait()
+	// use backupHandler.Wait() to wait for the job to finish or fail.
+	// You can use different context here, and if it is canceled
+	// backupClient.Backup(ctx, backupCfg, writers) context will be cancelled too.
+	err = backupHandler.Wait(ctx)
 	if err != nil {
 		log.Printf("Backup failed: %v", err)
 	}
@@ -130,8 +132,10 @@ func runRestore(ctx context.Context, c *backup.Client) {
 		panic(err)
 	}
 
-	// use restoreHandler.Wait() to wait for the job to finish or fail
-	err = restoreHandler.Wait()
+	// use restoreHandler.Wait() to wait for the job to finish or fail.
+	// You can use different context here, and if it is canceled
+	// backupClient.Restore(ctx, restoreCfg, streamingReader) context will be cancelled too.
+	err = restoreHandler.Wait(ctx)
 	if err != nil {
 		log.Printf("Restore failed: %v", err)
 	}

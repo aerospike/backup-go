87668f9646551f46209723ecf9ce419863bf5fe6
Dmitrii Neeman <dneeman@aerospike.com>
2024-08-29 16:47:14 +0300

    FMWK-411-ignore-record-error - comments fix

--- config_restore.go
+++ config_restore.go
@@ -71,8 +71,8 @@ type RestoreConfig struct {
 	// Amount of extra time-to-live to add to records that have expirable void-times.
 	// Must be set in seconds.
 	ExtraTTL int64
-	//  Ignore permanent record-specific error.
-	//  E.g.: AEROSPIKE_RECORD_TOO_BIG.
+	// Ignore permanent record-specific error.
+	// E.g.: AEROSPIKE_RECORD_TOO_BIG.
 	// By default, such errors are not ignored and restore terminates.
 	IgnoreRecordError bool
 }

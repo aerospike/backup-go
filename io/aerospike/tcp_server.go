d1fa9ca83f895673a9004c520c0328520261161c
Dmitrii Neeman <dneeman@aerospike.com>
2024-11-17 15:01:21 +0200

    xdr-support-tcp-server - linter - licenses

--- io/aerospike/tcp_server.go
+++ io/aerospike/tcp_server.go
@@ -1,3 +1,17 @@
+// Copyright 2024 Aerospike, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package aerospike
 
 import (
@@ -129,7 +143,6 @@ func (s *TCPServer) readRequest(conn net.Conn) ([]byte, error) {
 	)
 
 	return readBytes(conn, int(length))
-
 }
 
 // readBytes reading `length` number of bytes from `conn`.

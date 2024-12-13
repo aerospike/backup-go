ca5c71c6ac5ee77c6cc7707b4e9d6a9b32d64d04
Dmitrii Neeman <filkeith@users.noreply.github.com>
2024-12-13 14:36:24 +0200

    FMWK-598 Implement XDR data parser (#190)

--- internal/asinfo/info_client_test.go
+++ internal/asinfo/info_client_test.go
@@ -26,6 +26,16 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	testASLoginPassword = "admin"
+	testASNamespace     = "test"
+	testASDC            = "DC1"
+	testASHost          = "127.0.0.1"
+	testASPort          = 3000
+	testASRewind        = "all"
+	testXRdHostPort     = "127.0.0.1:3003"
+)
+
 func Test_parseAerospikeVersion(t *testing.T) {
 	type args struct {
 		versionStr string
@@ -1697,5 +1707,98 @@ func Test_parseInfoObject(t *testing.T) {
 		result, err := parseInfoObject(tt.obj, ":", "=")
 		require.Equal(t, tt.result, result)
 		require.Equal(t, tt.err, err)
+	}
+}
+
+func TestInfoCommander_EnableDisableXDR(t *testing.T) {
+	t.Parallel()
+
+	infoPolicy := a.NewInfoPolicy()
+	asPolicy := a.NewClientPolicy()
+	asPolicy.User = testASLoginPassword
+	asPolicy.Password = testASLoginPassword
+	client, aerr := a.NewClientWithPolicy(asPolicy, testASHost, testASPort)
+	require.NoError(t, aerr)
+
+	ic := NewInfoClientFromAerospike(client, infoPolicy)
+
+	err := ic.StartXDR(testASDC, testXRdHostPort, testASNamespace, testASRewind)
+	require.NoError(t, err)
+
+	err = ic.StopXDR(testASDC, testXRdHostPort, testASNamespace)
+	require.NoError(t, err)
+}
+
+func TestInfoCommander_BlockUnblockMRTWrites(t *testing.T) {
+	t.Parallel()
+
+	infoPolicy := a.NewInfoPolicy()
+	asPolicy := a.NewClientPolicy()
+	asPolicy.User = testASLoginPassword
+	asPolicy.Password = testASLoginPassword
+	client, aerr := a.NewClientWithPolicy(asPolicy, testASHost, testASPort)
+	require.NoError(t, aerr)
+
+	ic := NewInfoClientFromAerospike(client, infoPolicy)
+
+	err := ic.BlockMRTWrites(testASDC, testASNamespace)
+	require.NoError(t, err)
+
+	err = ic.UnBlockMRTWrites(testASDC, testASNamespace)
+	require.NoError(t, err)
+}
+
+func TestInfoCommander_parseResultResponse(t *testing.T) {
+	tests := []struct {
+		name     string
+		cmd      string
+		input    map[string]string
+		expected string
+		errMsg   string
+	}{
+		{
+			name:     "Command exists with successful response",
+			cmd:      "testCommand",
+			input:    map[string]string{"testCommand": "success"},
+			expected: "success",
+			errMsg:   "",
+		},
+		{
+			name:     "Command exists with failure response",
+			cmd:      "testCommand",
+			input:    map[string]string{"testCommand": "ERROR: command failed"},
+			expected: "",
+			errMsg:   "command testCommand failed: ERROR: command failed",
+		},
+		{
+			name:     "Command not found in map",
+			cmd:      "missingCommand",
+			input:    map[string]string{"testCommand": "success"},
+			expected: "",
+			errMsg:   "no response for command missingCommand",
+		},
+		{
+			name:     "Empty response map",
+			cmd:      "testCommand",
+			input:    map[string]string{},
+			expected: "",
+			errMsg:   "no response for command testCommand",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, err := parseResultResponse(tt.cmd, tt.input)
+			if result != tt.expected {
+				t.Errorf("expected result %v, got %v", tt.expected, result)
+			}
+			if err != nil {
+				if err.Error() != tt.errMsg {
+					t.Errorf("expected error message %v, got %v", tt.errMsg, err)
+				}
+			} else if tt.errMsg != "" {
+				t.Errorf("expected error message %v, got nil", tt.errMsg)
+			}
+		})
 	}
 }
--- io/aerospike/xdr/tcp_server.go
+++ io/aerospike/xdr/tcp_server.go
@@ -17,147 +17,404 @@ package xdr
 import (
 	"context"
 	"crypto/tls"
-	"encoding/binary"
+	"errors"
 	"fmt"
+	"io"
 	"log/slog"
 	"net"
+	"os"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/aerospike/backup-go/models"
 )
 
-type TCPServer struct {
-	address    string
-	readTimout time.Duration
-	tlsConfig  *tls.Config
-	// Results will be sent here.
-	resultChan chan *models.XDRToken
-	logger     *slog.Logger
+// TCPConfig contains tcp server config params.
+type TCPConfig struct {
+	// TCP server address.
+	Address string
+	// TLS config for secure connection.
+	TLSConfig *tls.Config
+	// Timeout in milliseconds for read operations.
+	ReadTimoutMilliseconds int64
+	// Timeout in milliseconds for write operations.
+	WriteTimeoutMilliseconds int64
+	// Results queue size.
+	ResultQueueSize int
+	// Ack messages queue size.
+	AckQueueSize int
+	// Max number of allowed simultaneous connection to server.
+	MaxConnections int
 }
 
+// NewDefaultTCPConfig returns default TCP Server config.
+func NewDefaultTCPConfig() *TCPConfig {
+	return &TCPConfig{
+		Address:                  ":8080",
+		ReadTimoutMilliseconds:   1000,
+		WriteTimeoutMilliseconds: 1000,
+		ResultQueueSize:          256,
+		AckQueueSize:             256,
+		MaxConnections:           100,
+	}
+}
+
+// TCPServer server for serving XDR connections.
+type TCPServer struct {
+	config *TCPConfig
+
+	// Fields for internal connection serving.
+	listener          net.Listener
+	activeConnections atomic.Int32
+	wg                sync.WaitGroup
+	cancel            context.CancelFunc
+
+	// Results will be sent here.
+	resultChan chan *models.XDRToken
+
+	logger *slog.Logger
+}
+
+// NewTCPServer returns a new tcp server for serving XDR connections.
 func NewTCPServer(
-	address string,
-	readTimeout time.Duration,
-	tlsConfig *tls.Config,
-	resultChan chan *models.XDRToken,
+	config *TCPConfig,
 	logger *slog.Logger,
 ) *TCPServer {
 	return &TCPServer{
-		address:    address,
-		readTimout: readTimeout,
-		tlsConfig:  tlsConfig,
-		resultChan: resultChan,
+		config:     config,
+		resultChan: make(chan *models.XDRToken, config.ResultQueueSize),
 		logger:     logger,
 	}
 }
 
-func (s *TCPServer) Start(ctx context.Context) error {
-	var (
-		listener net.Listener
-		err      error
-	)
+// Start launch tcp server for XDR.
+func (s *TCPServer) Start(ctx context.Context) (chan *models.XDRToken, error) {
+	var err error
 
-	if s.tlsConfig != nil {
-		listener, err = tls.Listen("tcp", s.address, s.tlsConfig)
+	// Redefine cancel function, so we can use it on Stop()
+	ctx, cancel := context.WithCancel(ctx)
+	s.cancel = cancel
+
+	// Create listener
+	if s.config.TLSConfig != nil {
+		s.listener, err = tls.Listen("tcp", s.config.Address, s.config.TLSConfig)
 		if err != nil {
-			return fmt.Errorf("failed to start tcp server with tls: %s", err)
+			return nil, fmt.Errorf("failed to start tcp server with tls: %w", err)
 		}
-
-		s.logger.Debug("started tcp server with tls")
 	} else {
-		listener, err = net.Listen("tcp", s.address)
+		s.listener, err = net.Listen("tcp", s.config.Address)
 		if err != nil {
-			return fmt.Errorf("failed to start tcp server without tls: %s", err)
+			return nil, fmt.Errorf("failed to start tcp server without tls: %w", err)
 		}
-
-		s.logger.Debug("started tcp server without tls")
 	}
 
-	defer listener.Close()
+	// Start connection acceptor
+	go s.acceptConnections(ctx)
+
+	s.logger.Info("server started",
+		slog.String("address", s.config.Address),
+		slog.Bool("tls", s.config.TLSConfig != nil))
+
+	return s.resultChan, nil
+}
+
+// Stop close listener and all communication channels.
+func (s *TCPServer) Stop() {
+	s.cancel()
+
+	if s.listener != nil {
+		if err := s.listener.Close(); err != nil {
+			s.logger.Warn("failed to close tcp server listener", slog.Any("error", err))
+		}
+	}
+
+	s.wg.Wait()
+
+	close(s.resultChan)
+
+	s.logger.Info("server shutdown complete")
+}
+
+// GetActiveConnections method for monitoring current state.
+func (s *TCPServer) GetActiveConnections() int32 {
+	return s.activeConnections.Load()
+}
+
+// acceptConnections serves connections, not more than maxConnections.
+// All connections over pool will be rejected.
+func (s *TCPServer) acceptConnections(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			conn, err := s.listener.Accept()
+			if err != nil {
+				if !errors.Is(err, net.ErrClosed) {
+					s.logger.Info("failed to accept connection", slog.Any("error", err))
+				}
+
+				continue
+			}
+			// Check if we have an opportunity to start new connections.
+			if s.activeConnections.Load() < int32(s.config.MaxConnections) {
+				// Increment connections counter.
+				s.activeConnections.Add(1)
+
+				go func() {
+					// Create and run handler.
+					handler := NewConnectionHandler(
+						conn,
+						s.resultChan,
+						s.config.AckQueueSize,
+						s.config.ReadTimoutMilliseconds,
+						s.config.WriteTimeoutMilliseconds,
+						s.logger,
+					)
+					// Handlers wait when all goroutines are finished.
+					handler.Start(ctx)
+					s.logger.Debug("connection finished",
+						slog.String("address", conn.RemoteAddr().String()))
+					s.activeConnections.Add(-1)
+				}()
+
+				s.logger.Debug("accepted new connection",
+					slog.String("address", conn.RemoteAddr().String()))
+			} else {
+				s.logger.Info("connection pool is full, rejecting TCP connection",
+					slog.String("address", conn.RemoteAddr().String()))
+
+				if err = conn.Close(); err != nil {
+					s.logger.Warn("failed to close connection",
+						slog.String("address", conn.RemoteAddr().String()))
+				}
+			}
+		}
+	}
+}
+
+// ConnectionHandler manages a single connection and its acknowledgment queue
+type ConnectionHandler struct {
+	conn net.Conn
+	// channel to send results.
+	resultChan chan *models.XDRToken
+	// Timeouts in nanoseconds.
+	readTimoutNano   int64
+	writeTimeoutNano int64
+	// To stop all goroutines from inside.
+	cancel context.CancelFunc
+	wg     sync.WaitGroup
+	// Queue to process received messages.
+	bodyQueue chan []byte
+	// Queue to process ack messages.
+	ackQueue chan []byte
+
+	// Cached values.
+	ackMsgSuccess []byte
+	ackMsgRetry   []byte
+	timeNow       int64
+
+	logger *slog.Logger
+}
+
+// NewConnectionHandler returns new connection handler.
+// For each connection must be created a separate handler.
+func NewConnectionHandler(
+	conn net.Conn,
+	resultChan chan *models.XDRToken,
+	ackQueueSize int,
+	readTimout int64,
+	writeTimeout int64,
+	logger *slog.Logger,
+) *ConnectionHandler {
+	return &ConnectionHandler{
+		conn:             conn,
+		resultChan:       resultChan,
+		readTimoutNano:   readTimout * 1_000_000,
+		writeTimeoutNano: writeTimeout * 1_000_000,
+		timeNow:          time.Now().UnixNano(),
+		bodyQueue:        make(chan []byte, ackQueueSize),
+		ackQueue:         make(chan []byte, ackQueueSize),
+		ackMsgSuccess:    NewAckMessage(AckOK),
+		ackMsgRetry:      NewAckMessage(AckRetry),
+		logger:           logger,
+	}
+}
+
+// Start launch goroutines to serve current connection.
+func (h *ConnectionHandler) Start(ctx context.Context) {
+	ctx, cancel := context.WithCancel(ctx)
+	h.cancel = cancel
+
+	h.wg.Add(4)
+
+	// This function serve h.timeNow field, to save each 100 milliseconds, current time.
+	// This time is used to update deadlines on write and read operations, to improve speed.
+	go func() {
+		defer h.wg.Done()
+
+		ticker := time.NewTicker(100 * time.Millisecond) // Update every 100ms
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case t := <-ticker.C:
+				atomic.StoreInt64(&h.timeNow, t.UnixNano())
+			}
+		}
+	}()
+
+	go func() {
+		defer h.wg.Done()
+		h.handleMessages(ctx)
+	}()
+
+	go func() {
+		defer h.wg.Done()
+		h.processMessage(ctx)
+	}()
+
+	go func() {
+		defer h.wg.Done()
+		h.handleAcknowledgments(ctx)
+	}()
+
+	// Wait and clean up when all routines exit.
+	h.wg.Wait()
+	h.cleanup()
+}
+
+// cleanup closes communication channels for current handler and closes connection itself.
+func (h *ConnectionHandler) cleanup() {
+	close(h.ackQueue)
+	close(h.bodyQueue)
+
+	if err := h.conn.Close(); err != nil {
+		h.logger.Warn("failed to close connection", slog.Any("error", err))
+	}
+}
+
+// handleMessages processes incoming messages
+func (h *ConnectionHandler) handleMessages(ctx context.Context) {
+	parser := NewParser(h.conn)
 
 	for {
 		select {
 		case <-ctx.Done():
-			s.logger.Debug("tcp server context done")
-			return nil
+			return
 		default:
-			conn, err := listener.Accept()
-			if err != nil {
-				s.logger.Error("failed to accept tcp connection", slog.Any("error", err))
-				continue
+			deadline := h.getDeadline(h.readTimoutNano)
+
+			if err := h.conn.SetReadDeadline(time.Unix(deadline, 0)); err != nil {
+				h.logger.Error("failed to set read deadline", slog.Any("error", err))
+
+				return
 			}
 
-			go s.handleConn(ctx, conn)
+			message, err := parser.Read()
+
+			switch {
+			case err == nil:
+			// ok.
+			case errors.Is(err, io.EOF):
+				// do nothing, wait for the next message.
+				continue
+			case os.IsTimeout(errors.Unwrap(err)):
+				// If timeout reached and the connection is closed, do nothing.
+				return
+			default:
+				h.logger.Error("failed to read message", slog.Any("error", err))
+				return
+			}
+
+			// Process message asynchronously
+			h.bodyQueue <- message
 		}
 	}
 }
 
-func (s *TCPServer) handleConn(ctx context.Context, conn net.Conn) {
-	defer conn.Close()
-	s.logger.Debug("handling tcp connection", slog.String("client", conn.RemoteAddr().String()))
-
+// processMessage serves h.bodyQueue. When a message is received, we try to parse it
+// and send it to h.resultChan, also ack messages is created for this message and sent to h.ackQueue.
+func (h *ConnectionHandler) processMessage(ctx context.Context) {
 	for {
-		if ctx.Err() != nil {
-			s.logger.Error("handle connection context", slog.Any("error", ctx.Err()))
+		select {
+		case <-ctx.Done():
 			return
-		}
+		case message := <-h.bodyQueue:
+			// Parse message.
+			aMsg, err := ParseAerospikeMessage(message)
+			if err != nil {
+				h.logger.Error("failed to parse aerospike message", slog.Any("error", err))
+				// If we have an error on parsing message, we send an ack message with retry.
+				h.ackQueue <- h.ackMsgRetry
 
-		if err := conn.SetDeadline(time.Now().Add(s.readTimout)); err != nil {
-			s.logger.Error("failed to set deadline",
-				slog.String("client", conn.RemoteAddr().String()),
-				slog.Duration("timeout", s.readTimout),
-			)
-		}
+				return
+			}
+			// Create aerospike key.
+			key, err := NewAerospikeKey(aMsg.Fields)
+			if err != nil {
+				h.logger.Error("failed to parse aerospike key", slog.Any("error", err))
+				// If we have an error on parsing message, we send an ack message with retry.
+				h.ackQueue <- h.ackMsgRetry
 
-		// read from connection.
-		req, err := s.readRequest(conn)
-		if err != nil {
-			s.logger.Error("failed to read request", slog.String("client", conn.RemoteAddr().String()))
-			continue
-		}
+				return
+			}
+			// Prepare payload.
+			// Reset xdr bit.
+			message = ResetXDRBit(message)
+			// Add headers.
+			payload := NewPayload(message)
+			// Create token XDRToken.
+			token := models.NewXDRToken(key, payload)
+			// Send XDRToken to results queue.
+			h.resultChan <- token
 
-		// parse request.
-		token := models.NewXDRToken(req)
-		s.resultChan <- token
-		// write response. If needed.
-		s.logger.Debug("parsed request",
-			slog.String("client", conn.RemoteAddr().String()),
-			slog.Any("token", token),
-		)
+			// Make acknowledgement.
+			h.ackQueue <- h.ackMsgSuccess
+		}
 	}
 }
 
-// readRequest example of implementation.
-// TODO: Will be replaced after protocol will be ready.
-func (s *TCPServer) readRequest(conn net.Conn) ([]byte, error) {
-	header, err := readBytes(conn, 4)
-	if err != nil {
-		return nil, err
+// handleAcknowledgments manages the async sending of acks.
+// Receive messages from h.ackQueue and write them to connection.
+func (h *ConnectionHandler) handleAcknowledgments(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case ack := <-h.ackQueue:
+			// Process each received ack message.
+			if err := h.sendAck(ack); err != nil {
+				h.logger.Error("failed to send ack", slog.Any("error", err))
+				// Close connection!
+				h.cancel()
+
+				return
+			}
+		}
 	}
-
-	length := binary.BigEndian.Uint32(header)
-
-	s.logger.Debug("received request",
-		slog.String("client", conn.RemoteAddr().String()),
-		slog.Any("length", length),
-	)
-
-	return readBytes(conn, int(length))
 }
 
-// readBytes reading `length` number of bytes from `conn`.
-func readBytes(conn net.Conn, length int) ([]byte, error) {
-	buffer := make([]byte, length)
-	total := 0
-
-	for total < length {
-		n, err := conn.Read(buffer[total:])
-		if err != nil {
-			return nil, err
-		}
-
-		total += n
+// sendAck updates connection deadline and writes an ack message to connection.
+func (h *ConnectionHandler) sendAck(ack []byte) error {
+	deadline := h.getDeadline(h.writeTimeoutNano)
+	if err := h.conn.SetWriteDeadline(time.Unix(0, deadline)); err != nil {
+		return fmt.Errorf("failed to set ack write deadline: %w", err)
 	}
 
-	return buffer, nil
+	// Write an ack message to connection.
+	if _, err := h.conn.Write(ack); err != nil {
+		return fmt.Errorf("error writing ack to connection: %w", err)
+	}
+
+	return nil
+}
+
+// getDeadline loads current time from h.timeNow and add timeout value.
+func (h *ConnectionHandler) getDeadline(timeout int64) int64 {
+	timeNow := atomic.LoadInt64(&h.timeNow)
+	return timeNow + timeout
 }

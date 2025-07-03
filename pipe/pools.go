c7fd3906a79dfcb977773c14e32b6384469a0ac3
Dmitrii Neeman <dneeman@aerospike.com>
2025-07-03 17:35:33 +0300

    APPS-1794-mem-leak-fix - comments fix

--- pipe/pools.go
+++ pipe/pools.go
@@ -84,7 +84,7 @@ func NewWriterPool[T models.TokenConstraint](writers []Writer[T], limiter *bandw
 
 // Close closing channels and cleaning links.
 func (p *Pool[T]) Close() {
-	// Nullify objects, so GC can free this memory,
+	// Nullify objects, so GC can free this memory.
 	p.Chains = nil
 	p.Inputs = nil
 	p.Outputs = nil

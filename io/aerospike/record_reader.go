a3925b257632ff8309cabca32e52783c90551aed
Dmitrii Neeman <dneeman@aerospike.com>
2025-08-14 14:47:12 +0300

    FMWK-806-scan-limiter - returned back parallel read

--- io/aerospike/record_reader.go
+++ io/aerospike/record_reader.go
@@ -325,7 +325,11 @@ func (r *RecordReader) startScan(ctx context.Context) {
 
 // processRecordSets processes the scan results from recordSets and sends them to the resultChan.
 func (r *RecordReader) processRecordSets(ctx context.Context) {
+	var wg sync.WaitGroup
+
+	// Order of this defers is important.
 	defer close(r.resultChan)
+	defer wg.Wait()
 
 	for {
 		select {
@@ -335,8 +339,13 @@ func (r *RecordReader) processRecordSets(ctx context.Context) {
 			if !ok {
 				return
 			}
+			// Add to wait group before starting processing.
+			wg.Add(1)
 
-			r.processRecords(ctx, set)
+			go func(set *a.Recordset) {
+				defer wg.Done()
+				r.processRecords(ctx, set)
+			}(set)
 		}
 	}
 }

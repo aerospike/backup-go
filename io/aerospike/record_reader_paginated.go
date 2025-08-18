40ae23f314f300b686cefac1194cc8b48e14bb34
akorotkov <akorotkov@aerospike.com>
2025-08-18 10:39:18 +0300

    fix tests

--- io/aerospike/record_reader_paginated.go
+++ io/aerospike/record_reader_paginated.go
@@ -117,8 +117,8 @@ func (r *PaginatedRecordReader) Read(ctx context.Context) (*models.Token, error)
 
 // startScan starts the scan for the RecordReader only for state save!
 func (r *PaginatedRecordReader) startScan() {
-	defer close(r.pageRecordsChan)
-	defer close(r.errChan)
+	defer close(r.errChan)         // err channel should be closed last.
+	defer close(r.pageRecordsChan) // result channel should nbe closed first.
 
 	scanPolicy := *r.config.scanPolicy
 	scanPolicy.FilterExpression = getScanExpression(scanPolicy.FilterExpression, r.config.timeBounds, r.config.noTTLOnly)

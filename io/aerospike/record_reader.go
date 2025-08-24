bc2304ebf6f50afc06a04d4c65e6ae5ae9f82841
Dmitrii Neeman <dneeman@aerospike.com>
2025-08-24 15:02:40 +0300

    FMWK-812-scan-set-exp - filter fix

--- io/aerospike/record_reader.go
+++ io/aerospike/record_reader.go
@@ -298,7 +298,7 @@ func getScanExpression(currentExpression *a.Expression, bounds models.TimeBounds
 	expressions := []*a.Expression{noMrtSetExpression()}
 
 	if len(sets) > 0 {
-		setsExps := make([]*a.Expression, len(sets))
+		setsExps := make([]*a.Expression, 0, len(sets))
 		for _, set := range sets {
 			setExp := a.ExpEq(a.ExpSetName(), a.ExpStringVal(set))
 			setsExps = append(setsExps, setExp)

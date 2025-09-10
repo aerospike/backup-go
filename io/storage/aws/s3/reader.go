7505b34e8e1ae0011cf3da16850deae8f8f8f9bf
Dmitrii Neeman <dneeman@aerospike.com>
2025-09-10 14:45:35 +0300

    APPS-1912-repeated-reader - change log level

--- io/storage/aws/s3/reader.go
+++ io/storage/aws/s3/reader.go
@@ -583,7 +583,7 @@ func (r *Reader) calculateTotalSize(ctx context.Context) {
 		size, num, err := r.calculateTotalSizeForPath(ctx, path)
 		if err != nil {
 			if r.Logger != nil {
-				r.Logger.Info("failed to calculate stats for path",
+				r.Logger.Warn("failed to calculate stats for path",
 					slog.String("path", path),
 					slog.Any("error", err),
 				)

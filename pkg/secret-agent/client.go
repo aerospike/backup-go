7ecf665dfe09a5cb45bcaf6a95b93d1f363bfaf3
Anton Korotkov <akorotkov@aerospike.com>
2026-01-25 10:48:23 +0200

    nolintlint

--- pkg/secret-agent/client.go
+++ pkg/secret-agent/client.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//nolint:revive // We want to use package name with underscore.
+//nolint:revive,nolintlint // We want to use package name with underscore.
 package secret_agent
 
 import (

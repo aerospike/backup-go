65ce9a38e9afb2d096d08be32ee90cf339fe8217
Anton Korotkov <106995168+korotkov-aerospike@users.noreply.github.com>
2026-04-09 19:02:05 +0300

    change cap logic

--- io/storage/aws/s3/writer.go
+++ io/storage/aws/s3/writer.go
@@ -238,9 +238,13 @@ func (w *s3Writer) Write(p []byte) (int, error) {
 	}
 
 	// Buffer Promotion. If we have a small buffer, grow it.
-	if w.buffer.Len() > 0 && w.buffer.Cap() < w.chunkSize {
-		w.buffer.Grow(w.chunkSize - w.buffer.Cap())
-		slog.Warn("Buffer size grow to", slog.Int("cap (mb)", w.buffer.Cap()/1024/1024))
+	if w.buffer.Cap() < w.chunkSize {
+		// This ensures that after this call, Cap >= chunkSize
+		w.buffer.Grow(w.chunkSize - w.buffer.Len())
+
+		slog.Warn("Buffer size grown",
+			slog.Int("new_cap_mb", w.buffer.Cap()/1024/1024),
+		)
 	}
 
 	// 3. The Write Loop

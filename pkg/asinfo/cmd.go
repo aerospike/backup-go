a071da9bf9813c54005777a5de5cb5e1bf421318
Dmitrii Neeman <dneeman@aerospike.com>
2026-06-18 16:08:03 +0300

    SERVER-898-cluster-stable - modified before after

--- pkg/asinfo/cmd.go
+++ pkg/asinfo/cmd.go
@@ -78,7 +78,7 @@ const (
 	cmdClusterStable       = "cluster-stable:size=%d;ignore-migrations=false;namespace=%s"
 
 	cmdServerBackup = "backup:namespace=%s;job-id=%s;object_storage_type=%s;s3-bucket=%s;" +
-		"s3-region=%s;s3-profile=%s;access-key=%s;secret-key=%s"
+		"s3-region=%s;s3-profile=%s;access-key=%s;secret-key=%s;modified-before=%s;modified-after=%s"
 	cmdServerRestore = "restore:namespace=%s;job-id=%s;object_storage_type=%s;s3-bucket=%s;" +
 		"s3-region=%s;s3-profile=%s;access-key=%s;secret-key=%s"
 	cmdServerPrepareRestore = "prepare-restore:namespace=%s;job-id=%s;"
